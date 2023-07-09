f360a85622df2900e75efa3ace227192e4f46105
Allan Tolentino <ajbtolentino@gmail.com>
2023-07-09 13:04:19 +0800

    Added nx and updated layout

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/core/models/IProduct';
 
 @Component({
   selector: 'app-catalog',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent {
+  products: Product[] = [];
 
+  constructor() {
+    for(let i =0; i<20; i++){
+      this.products.push({
+        id: i,
+        name: `Product ${i}`,
+        price: 40,
+        description: 'Test description'
+      })
+    }
+  }
 }
